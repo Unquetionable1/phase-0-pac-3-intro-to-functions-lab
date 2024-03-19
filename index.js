ac30949d209aba1867ae52ea0317dcf363f53d2b
Edmound <eddnjoori@gmail.com>
2024-03-19 11:07:38 +0300

    Adds functions

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ function logWhisper(string){
 
 
 function sayHiToHeadphonedRoommate(string){ 
-    if (string.toLowerCase() === string){
+    if (string.toLowerCase() == string){
         console.log("I can't hear you!");
     }
 
-    if (string.toUpperCase() === string){
+    if (string.toUpperCase() == string){
         console.log("YES INDEED!");
     }
 
-    if (string === "Let's have dinner together!") {
+    if (string =="Let's have dinner together!") {
         console.log("I would love to!");
     }
+    return;
 }
